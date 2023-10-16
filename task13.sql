c2a615eb793ac8589eb0c11b3d2fa59373bcf6ab
ArtyomKuzmin <aramas.artt@yandex.ru>
2023-10-16 12:55:24 +0400

    lr

--- task13.sql
+++
@@ -1,2 +0,0 @@
-use cd;
-select firstname, surname, joindate from members order by joindate desc limit 1;
--- task8.sql
+++
@@ -1,6 +0,0 @@
-use cd;
-select * from facilities where monthlymaintenance <100;
-/*Объекты с ДОРОГОЙ качественной стоимостью*/
-use cd;
-select * from facilities where monthlymaintenance >=100;
-/*Объекты с ДЕШЁВОЙ качественной стоимостью*/
